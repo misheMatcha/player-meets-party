3a63ce82fc7e443ce5f6e6a176d13116f54fff30
misheMatcha <mishe.vong@gmail.com>
2020-05-19 11:18:09 -0700

    Add user registration route

--- models/User.js
+++ models/User.js
@@ -14,105 +14,105 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
-  birthday: {
-    type: String,
-    required: true
-  },
-  state: {
-    type: String,
-    required: true
-  },
-  zip: {
-    type: Number,
-    required: true
-  },
-  gender: {
-    type: String
-  },
-  orientation: {
-    type: String
-  },
-  relationship: {
-    type: String
-  },
-  pronouns: {
-    type: String
-  },
-  height: {
-    type: Number
-  },
-  body_type: {
-    type: String
-  },
-  ethnicity: {
-    type: Array
-  },
-  languages: {
-    type: Array
-  },
-  politics: {
-    type: String
-  },
-  education: {
-    type: String
-  },
-  occupation: {
-    type: String
-  },
-  religion: {
-    type: String
-  },
-  sign: {
-    type: String
-  },
-  tobacco: {
-    type: Boolean
-  },
-  drinks: {
-    type: Boolean
-  },
-  marijuana: {
-    type: Boolean
-  },
-  kids: {
-    type: Boolean
-  },
-  pets: {
-    type: Boolean
-  },
-  gamelist: {
-    type: Array
-  },
-  game_genres: {
-    type: Array
-  },
-  platform: {
-    type: String
-  },
-  pref_gender: {
-    type: String
-  },
-  pref_distance: {
-    type: Number
-  },
-  pref_age: {
-    type: Number
-  },
-  pref_connections: {
-    type: String
-  },
-  profile_questions: {
-    type: Array
-  },
-  liked: {
-    type: Array
-  },
-  passed: {
-    type: Array
-  },
-  blocked: {
-    type: Array
-  },
+  // birthday: {
+  //   type: String,
+  //   required: true
+  // },
+  // state: {
+  //   type: String,
+  //   required: true
+  // },
+  // zip: {
+  //   type: Number,
+  //   required: true
+  // },
+  // gender: {
+  //   type: String
+  // },
+  // orientation: {
+  //   type: String
+  // },
+  // relationship: {
+  //   type: String
+  // },
+  // pronouns: {
+  //   type: String
+  // },
+  // height: {
+  //   type: Number
+  // },
+  // body_type: {
+  //   type: String
+  // },
+  // ethnicity: {
+  //   type: Array
+  // },
+  // languages: {
+  //   type: Array
+  // },
+  // politics: {
+  //   type: String
+  // },
+  // education: {
+  //   type: String
+  // },
+  // occupation: {
+  //   type: String
+  // },
+  // religion: {
+  //   type: String
+  // },
+  // sign: {
+  //   type: String
+  // },
+  // tobacco: {
+  //   type: Boolean
+  // },
+  // drinks: {
+  //   type: Boolean
+  // },
+  // marijuana: {
+  //   type: Boolean
+  // },
+  // kids: {
+  //   type: Boolean
+  // },
+  // pets: {
+  //   type: Boolean
+  // },
+  // gamelist: {
+  //   type: Array
+  // },
+  // game_genres: {
+  //   type: Array
+  // },
+  // platform: {
+  //   type: String
+  // },
+  // pref_gender: {
+  //   type: String
+  // },
+  // pref_distance: {
+  //   type: Number
+  // },
+  // pref_age: {
+  //   type: Number
+  // },
+  // pref_connections: {
+  //   type: String
+  // },
+  // profile_questions: {
+  //   type: Array
+  // },
+  // liked: {
+  //   type: Array
+  // },
+  // passed: {
+  //   type: Array
+  // },
+  // blocked: {
+  //   type: Array
+  // },
   date: {
     type: Date,
     default: Date.now
