9e69c7092cbc70acc768af81f6a46d76cfc4f2fc
misheMatcha <mishe.vong@gmail.com>
2020-05-18 17:16:53 -0700

    Add bodyParser and users api route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const users = require('./routes/api/users');
 
 const db = require('./config/keys').MONGO_URI;
 
@@ -11,8 +12,12 @@ mongoose
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Server test"));
+app.get('/', (req, res) => res.send('Server test'));
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
