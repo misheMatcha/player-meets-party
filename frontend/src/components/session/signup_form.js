2a571c0ab680b3b3acdde72b1c8a2d7949d9e087
misheMatcha <mishe.vong@gmail.com>
2020-05-20 20:44:17 -0700

    Add more styling for the rest of the signup inputs

--- frontend/src/components/session/signup_form.js
+++ frontend/src/components/session/signup_form.js
@@ -56,10 +56,13 @@ const SignupForm = props => {
         if(!user[field].length || user[field].length > 0) props.clearErrors()
         break;
       case 'email':
+        if(!user[field].length) props.clearErrors()
         break;
       case 'password':
+        if(!user[field].length || (user[field].length > 5 && user[field].length < 31)) props.clearErrors()
         break;
       case 'birthday':
+        if(!user[field].length) props.clearErrors()
         break;
       case 'state':
         break;
@@ -82,8 +85,7 @@ const SignupForm = props => {
           <p className="signup-form-header-title">About you</p>
         </div>
 
-
-        <div className="signup-form-section">
+        <div className="signup-form-section hide">
           <p className="signup-form-section-title">Welcome! Who are you?</p>
           <div className="signup-form-section-input-container">
             <div className="signup-form-section-field-details">
@@ -101,58 +103,103 @@ const SignupForm = props => {
             </label>
           </div>
         </div>
-{/* 
 
-        <div className="signup-form-section">
-          <p className="signup-form-section-title">Let's connect! What's your email?</p>
+        <div className="signup-form-section hide">
+          <p className="signup-form-section-title">Let's connect!<br/>What's your email?</p>
           <div className="signup-form-section-input-container">
             <div className="signup-form-section-field-details">
-              <p className="signup-form-section-field">Name</p>
-              <p className="signup-form-section-error">{props.errors.name}</p>
+              <p className="signup-form-section-field">Email</p>
+              <p className="signup-form-section-error">{props.errors.email}</p>
             </div>
             <label className="signup-form-section-label">
-              <input className="signup-form-section-input" type="text" placeholder='Kratos, Isabelle, etc...' onChange={event => {
-                handleInput(event, 'name');
-                checkErrors('name')
+              <input className="signup-form-section-input" type="email" placeholder='your.email@example.com' onChange={event => {
+                handleInput(event, 'email');
+                checkErrors('email')
                 }}/>
               <p className="fas fa-exclamation" style={
-                props.errors.name ? {opacity:1} : {opacity:0}
+                props.errors.email ? {opacity:1} : {opacity:0}
               }/>
             </label>
           </div>
         </div>
 
-
-        <div className="signup-form-section">
-          <p className="signup-form-section-title">Welcome! Who are you?</p>
+        <div className="signup-form-section hide">
+          <p className="signup-form-section-title">Create a password</p>
           <div className="signup-form-section-input-container">
             <div className="signup-form-section-field-details">
-              <p className="signup-form-section-field">Name</p>
-              <p className="signup-form-section-error">{props.errors.name}</p>
+              <p className="signup-form-section-field">Password</p>
+              <p className="signup-form-section-error">{props.errors.password}</p>
             </div>
             <label className="signup-form-section-label">
-              <input className="signup-form-section-input" type="text" placeholder='Kratos, Isabelle, etc...' onChange={event => {
-                handleInput(event, 'name');
-                checkErrors('name')
+              <input className="signup-form-section-input" type="password" placeholder='6 characters minimum' onChange={event => {
+                handleInput(event, 'password');
+                checkErrors('password')
                 }}/>
               <p className="fas fa-exclamation" style={
-                props.errors.name ? {opacity:1} : {opacity:0}
+                props.errors.password ? {opacity:1} : {opacity:0}
               }/>
             </label>
           </div>
-        </div> */}
+        </div>
 
+        <div className="signup-form-section hide">
+          <p className="signup-form-section-title">When's your birthday?</p>
+          <div className="signup-form-section-input-container">
+            <div className="signup-form-section-field-details">
+              <p className="signup-form-section-field">Birthday</p>
+              <p className="signup-form-section-error">{props.errors.birthday}</p>
+            </div>
+            <label className="signup-form-section-label">
+              <input className="signup-form-section-input" type="date" placeholder='6 characters minimum' onChange={event => {
+                handleInput(event, 'birthday');
+                checkErrors('birthday')
+                }}/>
+              <p className="fas fa-exclamation" style={
+                props.errors.birthday ? {opacity:1} : {opacity:0}
+              }/>
+            </label>
+          </div>
+        </div>
 
+        <div className="signup-form-section">
+          <p className="signup-form-section-title">Where you at?</p>
+          <div className="signup-form-section-input-container">
+            <div className="signup-form-section-input-wrap">
+              <div className="signup-form-section-input-state">
+                <div className="signup-form-section-field-details-location">
+                  <p className="signup-form-section-field">State</p>
+                  <p className="signup-form-section-error-location">{props.errors.state}</p>
+                </div>
+                <label className="signup-form-section-label">
+                  <input className="signup-form-section-input" type="text" placeholder='CA' onChange={event => {
+                    handleInput(event, 'state');
+                    checkErrors('state')
+                    }}/>
+                  <p className="fas fa-exclamation" style={
+                    props.errors.state ? {opacity:1} : {opacity:0}
+                  }/>
+                </label>
+              </div>
+              <div className="signup-form-section-input-zip">
+                <div className="signup-form-section-field-details-location">
+                  <p className="signup-form-section-field">Zip code</p>
+                  <p className="signup-form-section-error-location">{props.errors.zip}</p>
+                </div>
+                <label className="signup-form-section-label">
+                  <input className="signup-form-section-input" type="text" placeholder='94043' onChange={event => {
+                    handleInput(event, 'zip');
+                    checkErrors('zip')
+                    }}/>
+                  <p className="fas fa-exclamation" style={
+                    props.errors.zip ? {opacity:1} : {opacity:0}
+                  }/>
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
 
-
-
-
-
-
-
-        
-       
-        <button type="submit">submit</button>
+        <button className="signup-form-button" type="submit"><p className="signup-form-button-p">SIGN UP</p></button>
       </form>
     </div>
   );
