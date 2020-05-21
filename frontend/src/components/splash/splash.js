181ff92486950341de6b9fd6dd5104df3db67afb
misheMatcha <mishe.vong@gmail.com>
2020-05-21 13:39:52 -0700

    Restructure splash for keyframes overlay and make navbar protected route only

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -4,56 +4,69 @@ const Splash = props => {
   return(
     <div className='splash'>
 
+      <div className='splash-main'>
+        <div className='splash-bg'>
+          <div className='splash-bg-colors'/>
+          <div className='splash-bg-images'/>
+        </div>
+        <div className='splash-content'>
+          fg
+        </div>
+      </div>
+      
       <div className='splash-footer'>
-        <div className='splash-footer-join'>
-          <button className='splash-footer-join-btn'>JOIN PMP</button>
-          <p className='splash-footer-join-copyright'>© PMP 2020</p>
-        </div>
-        <div className='splash-footer-section'>
-          <div className='splash-footer-section-title'>COMPANY</div>
-          <div className="splash-footer-section-links">
-            <div className='splash-footer-section-link'>About</div>
-            <div className='splash-footer-section-link'>Careers</div>
-            <div className='splash-footer-section-link'>Press</div>
-            <div className='splash-footer-section-link'>Ad Choices</div>
+        <div className='splash-footer-container'>
+          <div className='splash-footer-join'>
+            <button className='splash-footer-join-btn'>JOIN PMP</button>
+            <p className='splash-footer-join-copyright'>© PMP 2020</p>
           </div>
-        </div>
-        <div className='splash-footer-section'>
-          <div className='splash-footer-section-title'>CONDITIONS</div>
-          <div className="splash-footer-section-links">
-            <div className='splash-footer-section-link'>Privacy</div>
-            <div className='splash-footer-section-link'>Cookies</div>
-            <div className='splash-footer-section-link'>Terms</div>
-            <div className='splash-footer-section-link'>Community Guidelines</div>
+          <div className='splash-footer-section'>
+            <div className='splash-footer-section-title'>COMPANY</div>
+            <div className='splash-footer-section-links'>
+              <div className='splash-footer-section-link'>About</div>
+              <div className='splash-footer-section-link'>Careers</div>
+              <div className='splash-footer-section-link'>Press</div>
+              <div className='splash-footer-section-link'>Ad Choices</div>
+            </div>
           </div>
-        </div>
-        <div className='splash-footer-section'>
-          <div className='splash-footer-section-title'>CONTACT</div>
-          <div className="splash-footer-section-links">
-            <div className='splash-footer-section-link'>Support</div>
-            <div className='splash-footer-section-link'>Security</div>
-            <div className='splash-footer-section-link'>Safety Tips</div>
+          <div className='splash-footer-section'>
+            <div className='splash-footer-section-title'>CONDITIONS</div>
+            <div className='splash-footer-section-links'>
+              <div className='splash-footer-section-link'>Privacy</div>
+              <div className='splash-footer-section-link'>Cookies</div>
+              <div className='splash-footer-section-link'>Terms</div>
+              <div className='splash-footer-section-link'>Community Guidelines</div>
+            </div>
           </div>
-        </div>
-        <div className='splash-footer-section'>
-          <div className='splash-footer-section-title'>FOLLOW</div>
-          <div className="splash-footer-section-links">
-            <div className='splash-footer-section-link'>Blog</div>
-            <div className='splash-footer-section-link'>Tech Blog</div>
-            <div className='splash-footer-section-link'>Facebook</div>
-            <div className='splash-footer-section-link'>Instagram</div>
-            <div className='splash-footer-section-link'>Twitter</div>
+          <div className='splash-footer-section'>
+            <div className='splash-footer-section-title'>CONTACT</div>
+            <div className='splash-footer-section-links'>
+              <div className='splash-footer-section-link'>Support</div>
+              <div className='splash-footer-section-link'>Security</div>
+              <div className='splash-footer-section-link'>Safety Tips</div>
+            </div>
           </div>
-        </div>
-        <div className='splash-footer-section'>
-          <div className='splash-footer-section-title'>SPECIAL</div>
-          <div className="splash-footer-section-links">
-            <div className='splash-footer-section-link'>Vegetarian Dating</div>
-            <div className='splash-footer-section-link'>Redeem Promotion</div>
-            <div className='splash-footer-section-link'>Download App</div>
+          <div className='splash-footer-section'>
+            <div className='splash-footer-section-title'>FOLLOW</div>
+            <div className='splash-footer-section-links'>
+              <div className='splash-footer-section-link'>Blog</div>
+              <div className='splash-footer-section-link'>Tech Blog</div>
+              <div className='splash-footer-section-link'>Facebook</div>
+              <div className='splash-footer-section-link'>Instagram</div>
+              <div className='splash-footer-section-link'>Twitter</div>
+            </div>
+          </div>
+          <div className='splash-footer-section'>
+            <div className='splash-footer-section-title'>SPECIAL</div>
+            <div className='splash-footer-section-links'>
+              <div className='splash-footer-section-link'>Vegetarian Dating</div>
+              <div className='splash-footer-section-link'>Redeem Promotion</div>
+              <div className='splash-footer-section-link'>Download App</div>
+            </div>
           </div>
         </div>
       </div>
+
     </div>
   );
 };
