7a4641b9501f2c9d4f5c271f8e0a34c2f3841c02
misheMatcha <mishe.vong@gmail.com>
2020-05-21 16:21:59 -0700

    Update modal reducer

--- frontend/src/reducers/modal_reducer.js
+++ frontend/src/reducers/modal_reducer.js
@@ -1,6 +1,11 @@
-const ModalReducer = (state, action) => {
+import { OPEN_MODAL, CLOSE_MODAL } from '../actions/modal_actions';
+
+const ModalReducer = (state = null, action) => {
   switch(action.type){
-    case 'test':
+    case OPEN_MODAL:
+      return action.modal;
+    case CLOSE_MODAL:
+      return null;
     default:
       return state;
   }
