851e9c58b1ad99f847fe317b44016b7b025a2791
misheMatcha <mishe.vong@gmail.com>
2020-05-23 00:59:16 -0700

    Add styling to misc section of navbar

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -18,21 +18,21 @@ const Navbar = props => {
         <div className='navbar-content'>
           <p className='navbar-content-logo'>PMP</p>
           <div className='navbar-content-links-wrap'>
-            <NavLink className='navbar-content-link' activeClassName='navbar-content-link-active' to='/doubletake'><p className='far fa-images'/> DoubleTake</NavLink>
-            <NavLink className='navbar-content-link' activeClassName='navbar-content-link-active' to='/discover'><p className='far fa-compass'/> Discover</NavLink>
-            <NavLink className='navbar-content-link' activeClassName='navbar-content-link-active' to='/search'><p className='fas fa-search'/> Search</NavLink>
-            <NavLink className='navbar-content-link' activeClassName='navbar-content-link-active' to='/likes'><p className='far fa-heart'/> Likes</NavLink>
-            <NavLink className='navbar-content-link' activeClassName='navbar-content-link-active' to='/messages'><p className='far fa-comment-alt'/> Messages</NavLink>
+            <NavLink className='navbar-content-link' activeClassName='navbar-content-link-active' to='/doubletake'><div className='far fa-images'/> DoubleTake</NavLink>
+            <NavLink className='navbar-content-link' activeClassName='navbar-content-link-active' to='/discover'><div className='far fa-compass'/> Discover</NavLink>
+            <NavLink className='navbar-content-link' activeClassName='navbar-content-link-active' to='/search'><div className='fas fa-search'/> Search</NavLink>
+            <NavLink className='navbar-content-link' activeClassName='navbar-content-link-active' to='/likes'><div className='far fa-heart'/> Likes</NavLink>
+            <NavLink className='navbar-content-link' activeClassName='navbar-content-link-active' to='/messages'><div className='far fa-comment-alt'/> Messages</NavLink>
           </div>
         </div>
         <div className='navbar-misc'>
           <div className='navbar-profile'>
             <img className='navbar-profile-img' src={placeholder.profile}/>
             <p className='navbar-profile-user'>{placeholder.username}</p>
-            <p className='fas fa-angle-down'/>
+            <div className='fas fa-angle-down'/>
           </div>
-          <button className='navbar-join'>JOIN A-LIST</button>
-          <button className='navbar-boost'><div className='fas fa-bolt'/> BOOST</button>
+          <button className='navbar-btn nbjoin'>JOIN A-LIST</button>
+          <button className='navbar-btn nbboost'><div className='fas fa-bolt'/> BOOST</button>
         </div>
       </div>
     </div>
