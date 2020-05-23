2538a9ba75285b8b77a9cd0b3a7dee9251b28edf
misheMatcha <mishe.vong@gmail.com>
2020-05-23 14:49:14 -0700

    Small tweaks

--- frontend/src/components/boost/boost.js
+++ frontend/src/components/boost/boost.js
@@ -15,16 +15,16 @@ const Boost = ({closeModal}) => {
         </div>
         <div className='boost-options'>
           <form className='boost-form'>
-            <label className='boost-form-section'>
+            <label for='10' className='boost-form-section'>
               <input className='boost-form-radio' type='radio' id='10' name='boost' value='10'/>
               <p className='boost-form-amt'>10 Boosts</p> for $1.69 each
             </label>
-            <label className='boost-form-section'>
-              <input className='boost-form-radio' type='radio' id='10' name='boost' value='10'/>
+            <label for='5' className='boost-form-section'>
+              <input className='boost-form-radio' type='radio' id='5' name='boost' value='10'/>
               <p className='boost-form-amt'>5 Boosts</p> for $1.79 each
             </label>
-            <label className='boost-form-section'>
-              <input className='boost-form-radio' type='radio' id='10' name='boost' value='10'/>
+            <label for='1' className='boost-form-section'>
+              <input className='boost-form-radio' type='radio' id='1' name='boost' value='10'/>
               <p className='boost-form-amt'>1 Boosts</p> for $1.99 each
             </label>
             <button className='boost-submit' type='button' onClick={() => console.log('submitted')}>Boost now</button>
