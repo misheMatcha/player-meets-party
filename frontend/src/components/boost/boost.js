983621607948407d583a95cf0dfff6189d8fe5d0
misheMatcha <mishe.vong@gmail.com>
2020-05-23 13:09:30 -0700

    Add basic dropdown hover

--- frontend/src/components/boost/boost.js
+++ frontend/src/components/boost/boost.js
@@ -14,7 +14,7 @@ const Boost = ({closeModal}) => {
           </div>
         </div>
         <div className='boost-options'>
-          <form className='boost-form' onSubmit={() => console.log('submitted')}>
+          <form className='boost-form'>
             <label className='boost-form-section'>
               <input className='boost-form-radio' type='radio' id='10' name='boost' value='10'/>
               <p className='boost-form-amt'>10 Boosts</p> for $1.69 each
@@ -27,7 +27,7 @@ const Boost = ({closeModal}) => {
               <input className='boost-form-radio' type='radio' id='10' name='boost' value='10'/>
               <p className='boost-form-amt'>1 Boosts</p> for $1.99 each
             </label>
-            <button className='boost-submit' type='submit'>Boost now</button>
+            <button className='boost-submit' type='button' onClick={() => console.log('submitted')}>Boost now</button>
             <button className='boost-close' onClick={() => closeModal()}>No thanks</button>
           </form>
         </div>
