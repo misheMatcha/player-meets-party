362940e6cde9bab22bce9a901fdd5ae1993fb3de
misheMatcha <mishe.vong@gmail.com>
2020-05-25 16:48:45 -0700

    Add dynamic document title for profile. Create separate icons css for targeted styling

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Profile = props => {
   const placeholder = {
@@ -17,13 +17,23 @@ const Profile = props => {
       aboutme: ['My self-summary', 'Favorite thing about the place I live', 'Me, a Haiku', 'Most people that know me would say I\'m', 'Favorite memory from my childhood', 'Thing\'s I am not']
     };
 
+    useEffect(() => {
+      setTitle();
+      return(() => {
+      });
+    }, []);
+
+    const setTitle = () => {
+      document.title = `${placeholder.username} / ${placeholder.age} / ${placeholder.location}`;
+    };
+
   return(
     <div className='profile'>
       <div className='profile-basics'>
         <div className='profile-basics-info'>
           <img className='profile-basics-img' src={placeholder.profile} alt='profile'/>
           <div className='profile-basics-details'>
-            <div className='profile-basics-user'>{placeholder.username} <div className='fas fa-pencil-alt'/></div>
+            <div className='profile-basics-user'>{placeholder.username} <i className='fas fa-pencil-alt'/></div>
             <div className='profile-basics-asl'>
               <p className='profile-basics-asl-age'>{placeholder.age}</p>
               <p className='profile-basics-asl-spacer'>•</p>
