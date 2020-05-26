a528132e513575d85189d2e971b0b077656ff725
misheMatcha <mishe.vong@gmail.com>
2020-05-25 18:59:40 -0700

    Add single user and edit user actions

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -1,11 +1,25 @@
 import * as UserApiUtil from '../util/user_api_util';
 export const RECEIVE_ALL_USERS = 'RECEIVE_ALL_USERS';
+export const RECEIVE_USER = 'RECEIVE_USER';
 
 const receiveAllUsers = users => ({
   type: RECEIVE_ALL_USERS,
   users
 });
 
+const receiveUser = user => ({
+  type: RECEIVE_USER,
+  user
+});
+
 export const requestAllUsers = () => dispatch => UserApiUtil.fetchUsers()
   .then(users => dispatch(receiveAllUsers(users)))
+  .catch(err => console.log(err));
+
+export const requestUser = user_id => dispatch => UserApiUtil.fetchUser(user_id)
+  .then(user => dispatch(receiveUser(user)))
+  .catch(err => console.log(err));
+
+export const editUser = user => dispatch => UserApiUtil.updateUser(user)
+  .then(modifiedUser => dispatch(receiveUser(modifiedUser)))
   .catch(err => console.log(err));
