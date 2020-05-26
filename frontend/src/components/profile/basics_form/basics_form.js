d8612afaa2de7a3a4387b4b1792dbc03bf794ff3
misheMatcha <mishe.vong@gmail.com>
2020-05-26 15:34:50 -0700

    Add orientation and gender arrays for multi click options

--- frontend/src/components/profile/basics_form/basics_form.js
+++ frontend/src/components/profile/basics_form/basics_form.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const BasicsForm = props => {
+  const orientationOpts = ['Straight', 'Gay', 'Bisexual', 'Asexual', 'Demisexual', 'Heteroflexible', 'Homoflexible', 'Lesbian', 'Pansexual', 'Queer', 'Questioning'];
+  const genderOpts = ['Woman', 'Man', 'Agender', 'Androgynous', 'Bigender', 'Cis Man', 'Cis Woman', 'Genderfluid', 'Genderqueer', 'Gender Nonconforming', 'Hijra', 'Intersex', 'Non-binary', 'Other', 'Pangender', 'Transfeminine', 'Transgender', 'Transmasculine', 'Transsexual', 'Trans Man', 'Trans Woman', 'Two Spirit'];
+
   return(
     <div className='basics-form'>
       <div className='basics-form-header'>
@@ -10,13 +13,34 @@ const BasicsForm = props => {
         </div>
         </div>
       <div className='basics-form-content'>
-        <p className=''>I am a...</p>
-        <label className=''>
-          <input className='' type='text' placeholder='orientation'/>
-        </label>
-        <label className=''>
-          <input className='' type='text' placeholder='gender'/>
-        </label>
+        <p className='basics-form-content-title'>I am a...</p>
+
+        <div className='basics-form-content-tag-container'>
+          <p className='basics-form-content-tag-title'>Select up to 5</p>
+          <div className='basics-form-content-tag-list'>
+            {
+              orientationOpts.map((orienOpt, idx) => {
+                return(
+                  <button key={idx} className='basics-form-content-tag'>{orienOpt}</button>
+                );
+              })
+            }
+          </div>
+          <div className='basics-form-content-tag-actions'>
+            <button className='basics-form-content-tag-actions-cont'>Continue</button>
+            <button className='basics-form-content-tag-actions-cancel'>Cancel</button>
+          </div>
+        </div>
+
+        <div className=''>
+          <label className=''>
+            <input className='' type='text' placeholder='orientation'/>
+          </label>
+          <label className=''>
+            <input className='' type='text' placeholder='gender'/>
+          </label>
+        </div>
+
       </div>
     </div>
   );
