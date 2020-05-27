c70ab1d4ec3cdca3fbccfbd75b4bae312a5f0eda
misheMatcha <mishe.vong@gmail.com>
2020-05-26 23:18:01 -0700

    Complete structure for basics

--- frontend/src/components/profile/basics_form/basics_form.js
+++ frontend/src/components/profile/basics_form/basics_form.js
@@ -7,6 +7,7 @@ const BasicsForm = props => {
   const genderOpts = ['Woman', 'Man', 'Agender', 'Androgynous', 'Bigender', 'Cis Man', 'Cis Woman', 'Genderfluid', 'Genderqueer', 'Gender Nonconforming', 'Hijra', 'Intersex', 'Non-binary', 'Other', 'Pangender', 'Transfeminine', 'Transgender', 'Transmasculine', 'Transsexual', 'Trans Man', 'Trans Woman', 'Two Spirit'];
   const user = props.user;
   const gender = user ? user.gender : 'loading...';
+  const orientation = user ? user.orientation : 'loading...';
 
   return(
     <div className='basics-form'>
@@ -16,7 +17,7 @@ const BasicsForm = props => {
           <i className="fas fa-times"/>
         </div>
       </div>
-      <form className='basics-form-content'>
+      <form className='basics-form-content' onSubmit={() => console.log('submitted')}>
         <p className='basics-form-content-title'>I am a...</p>
 
         <div className={`basics-form-content-tag-container ${hideGenderTags ? 'hide' : ''}`}>
@@ -44,7 +45,7 @@ const BasicsForm = props => {
                 <option value='Straight' selected='selected'>Straight</option>
                 <option value='Gay'>Gay</option>
                 <option value='Bisexual'>Bisexual</option>
-                <option value='---'>More options</option>
+                <option value='Options'>More options</option>
               </select>
             </div>
             <div className='basics-form-content-inputs-list-arrow'>
@@ -58,6 +59,32 @@ const BasicsForm = props => {
             <p className={`basics-form-content-inputs-button-gender ${gender === 'Gender' ? '' : 'black'}`}>{gender}</p>
             <i className='fas fa-pencil-alt'/>
             </button>
+        </div>
+
+        <div className='basics-form-content-relationship'>
+          <p className='basics-form-content-title'>Relationship Type</p>
+          <div className='basics-form-content-inputs' style={{ display: !hideGenderTags ? 'none' : 'flex' }}>
+
+            <div className='basics-form-content-inputs-list'>
+              <div className='select'>
+                <select name='orientation' id='orientation'>
+                  <option value='Monogomous' selected='selected'>Monogomous</option>
+                  <option value='Non-monogomous'>Non-monogomous</option>
+                  <option value='Open to either'>Open to either</option>
+                </select>
+              </div>
+              <div className='basics-form-content-inputs-list-arrow'>
+                <i className="fas fa-chevron-down" />
+              </div>
+            </div>
+
+          </div>
+          <p className='basics-form-content-monogomy'>You will see and be seen by people who are monogamous and open to monogamy. Learn more.</p>
+        </div>
+        
+        <div className='basics-form-buttons'>
+          <button className='basics-form-buttons-temp bfsubmit' type='submit'>Save</button>
+          <button className='basics-form-buttons-temp bfcancel' type='button' onClick={() => props.closeModal()}>Cancel</button>
         </div>
 
       </form>
