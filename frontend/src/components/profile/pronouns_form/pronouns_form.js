7baf6f13b5cd72718943823f94ccdbafdbca2ce2
misheMatcha <mishe.vong@gmail.com>
2020-05-28 22:12:40 -0700

    Add structure to form

--- frontend/src/components/profile/pronouns_form/pronouns_form.js
+++ frontend/src/components/profile/pronouns_form/pronouns_form.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const PronounsForm = props => {
+  const [pronouns, setPronouns] = useState('');
+
+  useEffect(() => {
+    console.log(pronouns)
+    return () => {
+    };
+  });
+
   return(
     <div className='modal-form'>
       <div className='modal-form-header'>
@@ -11,6 +19,17 @@ const PronounsForm = props => {
           <i className="fas fa-times" />
         </div>
       </div>
+      <form className='modal-form-content'>
+        <p className='modal-form-content-title'>Pronouns</p>
+        {/* refactor later to have a dropdown as well as input */}
+        <label className='modal-form-content-label'>
+          <input className='pronouns-input' type='text' placeholder='Enter your own' value={pronouns} onChange={event => setPronouns(event.target.value)} />
+        </label>
+        <div className='modal-form-buttons-wrap'>
+          <button className='modal-form-buttons mfb-submit'>Save</button>
+          <button className='modal-form-buttons mfb-cancel'>Cancel</button>
+        </div>
+      </form>
     </div>
   );
 };
