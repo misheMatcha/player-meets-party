069df814b51e648e3c74bb09bb510a37b860fff0
misheMatcha <mishe.vong@gmail.com>
2020-06-13 01:46:07 -0700

    Refactor helpers into single function

--- frontend/src/components/coming_soon.jsx
+++ frontend/src/components/coming_soon.jsx
@@ -16,31 +16,64 @@ const ComingSoon = props => {
 
     return timeLeft;
   };
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft('12-02-2020'));
+  
+  const setDisplayOptions = component => {
+    let options = {};
+    switch(component){
+      case '/discover':
+        options = {
+          titleClass: 'coming-soon-header-discover',
+          imgClass: 'csc-discover',
+          bottomBorder: 'csc-discover-border',
+          date: '2020/08/02'
+        };
+        return options;
+      case '/search':
+        options = {
+          titleClass: 'coming-soon-header-search',
+          imgClass: 'csc-search',
+          bottomBorder: 'csc-search-border',
+          date: '2020/09/02'
+        };
+        return options;
+      case '/who-likes-you':
+        options = {
+          titleClass: 'coming-soon-header-likes',
+          imgClass: 'csc-likes',
+          bottomBorder: 'csc-likes-border',
+          date: '2020/07/02'
+        };
+        return options;
+      default:
+        return '';
+    }
+  };
+  const displayOptions = setDisplayOptions(props.history.location.pathname);
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(displayOptions.date));
 
   useEffect(() => {
     setTimeout(() => {
-      setTimeLeft(calculateTimeLeft('12-02-2020'));
+      setTimeLeft(calculateTimeLeft(displayOptions.date));
     }, 1000);
   });
   
-  return <div className='coming-soon-container'>
+  return <div className={`coming-soon-container ${displayOptions.imgClass}`}>
     <div className='coming-soon'>
-      <p className='coming-soon-header'>Coming soon</p>
+      <p className={displayOptions.titleClass}>coming soon</p>
       <div className='coming-soon-countdown'>
-        <div className='coming-soon-countdown-time'>
+        <div className={`coming-soon-countdown-time ${displayOptions.bottomBorder}`}>
           <p>{timeLeft.days}</p>
           <p className='coming-soon-countdown-increment'>DAYS</p>
         </div>
-        <div className='coming-soon-countdown-time'>
+        <div className={`coming-soon-countdown-time ${displayOptions.bottomBorder}`}>
           <p>{timeLeft.hours}</p>
           <p className='coming-soon-countdown-increment'>HOURS</p>
         </div>
-        <div className='coming-soon-countdown-time'>
+        <div className={`coming-soon-countdown-time ${displayOptions.bottomBorder}`}>
           <p>{timeLeft.minutes}</p>
           <p className='coming-soon-countdown-increment'>MINUTES</p>
         </div>
-        <div className='coming-soon-countdown-time'>
+        <div className={`coming-soon-countdown-time ${displayOptions.bottomBorder}`}>
           <p>{timeLeft.seconds}</p>
           <p className='coming-soon-countdown-increment'>SECONDS</p>
         </div>
