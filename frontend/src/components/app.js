e64517090ff9c151b8849aa12923ba11963ef341
misheMatcha <mishe.vong@gmail.com>
2020-06-12 21:24:16 -0700

    Add coming soon component

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -8,6 +8,7 @@ import SignupForm from './session/signup_form_container';
 import Splash from './splash/splash_container';
 import Doubletake from './doubletake/doubletake_container';
 import Profile from './profile/profile_container';
+import ComingSoon from './coming_soon.jsx';
 
 const App = () => (
   <div className='app'>
@@ -17,6 +18,9 @@ const App = () => (
       <AuthRoute exact path='/' component={Splash}/>
       <AuthRoute exact path='/signup' component={SignupForm}/>
       <ProtectedRoute exact path='/doubletake' component={Doubletake}/>
+      <ProtectedRoute exact path='/discover' component={ComingSoon}/>
+      <ProtectedRoute exact path='/search' component={ComingSoon}/>
+      <ProtectedRoute exact path='/who-likes-you' component={ComingSoon}/>
       <ProtectedRoute exact path='/profile/:id' component={Profile}/>
     </Switch>
   </div>
---
+++ frontend/src/components/coming_soon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ComingSoon = props => {
+  return <div className='coming-soon'>
+    <p>Feature coming soon</p>
+  </div>
+};
+
+export default ComingSoon;
